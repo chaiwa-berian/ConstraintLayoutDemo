1dc40b01665e72afb21255d2c07b04ce416e58e9
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-04-29 02:35:08 +0200

    Add UI Initialization code

--- app/src/main/java/com/bantulogic/learn/constraintlayoutgroupdemo/MainActivity.java
+++ app/src/main/java/com/bantulogic/learn/constraintlayoutgroupdemo/MainActivity.java
@@ -2,12 +2,38 @@ package com.bantulogic.learn.constraintlayoutgroupdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.security.acl.Group;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+    android.support.constraint.Group group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        group = findViewById(R.id.group);
+
+        button.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (group.getVisibility() == View.VISIBLE){
+            group.setVisibility(View.VISIBLE);
+            button.setText(R.string.hide_details);
+
+        }
+        else {
+            group.setVisibility(View.GONE);
+            button.setText(R.string.show_details);
+        }
     }
 }
