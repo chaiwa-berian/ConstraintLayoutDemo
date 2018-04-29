ed0fd650a4551d10c35e430144e2c1694772da61
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-04-29 13:45:44 +0200

    Add Constraint Layout by Rebecca Franks, GDG17-Johannesburg

--- app/src/main/java/com/bantulogic/learn/constraintlayoutgroupdemo/MainActivity.java
+++ app/src/main/java/com/bantulogic/learn/constraintlayoutgroupdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bantulogic.learn.constraintlayoutgroupdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import java.security.acl.Group;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button button;
+    Button layoutButton;
     android.support.constraint.Group group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +19,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        layoutButton = findViewById(R.id.buttonLoadAnotherLayout);
         group = findViewById(R.id.group);
 
         button.setOnClickListener(this);
+        layoutButton.setOnClickListener(this);
 
 
     }
 
     @Override
     public void onClick(View view) {
-        if (group.getVisibility() == View.GONE){
-            group.setVisibility(View.VISIBLE);
-            button.setText(R.string.hide_details);
+        switch (view.getId()){
+            case R.id.button:
+                if (group.getVisibility() == View.GONE){
+                    group.setVisibility(View.VISIBLE);
+                    button.setText(R.string.hide_details);
 
+                }
+                else {
+                    group.setVisibility(View.GONE);
+                    button.setText(R.string.show_details);
+                }
+            break;
+            case R.id.buttonLoadAnotherLayout:
+                Intent intent = new Intent();
+                intent.setClass(this,ConstraintLayoutActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
         }
-        else {
-            group.setVisibility(View.GONE);
-            button.setText(R.string.show_details);
-        }
+
     }
 }
